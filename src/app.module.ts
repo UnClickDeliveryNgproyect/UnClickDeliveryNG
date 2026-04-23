743a24681152870b4efb6cb30766d53b8dbb527b
LuisGonzalezzz <rosalesluis21042004@gmail.com>
2026-04-23 00:43:06 -0600

    conecting db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
-
+import { UsersModule } from './modules/users/users.module';
+import { config } from 'process';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+],
   controllers: [],
   providers: [],
 })
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@nestjs/common";
 import { UsersController } from "./controllers/users.controller";
 import { UsersService } from "./services/users.service";
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./entities/users.entity";
 
 @Module({
+    imports: [TypeOrmModule.forFeature([User]),
+    UsersModule,ConfigModule.forRoot()],
     controllers: [UsersController],
-    providers: [UsersService]
+    providers: [UsersService],
+    exports: [UsersService]
 })
 export class UsersModule {}
